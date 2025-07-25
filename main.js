8661a92aae05a4e8a83f5473e6cfac829818624a
James Hou <james.william.hou@gmail.com>
2025-07-25 13:40:33 -0700

    v5.4.1 model changes

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const initModelViewer = () => {
   
   // Create scene
   const scene = new THREE.Scene();
-  scene.background = new THREE.Color(0x171717);
+  scene.background = new THREE.Color(0x525252);
   
   // Create camera
   const camera = new THREE.PerspectiveCamera(45, container.clientWidth / container.clientHeight, 0.1, 1000);
